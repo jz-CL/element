2169c543a13bd9dd11fcc9bf8b53ac465d9996f1
Janusz Zychowicz <janusz.zychowicz@gmail.com>
2023-03-24 02:37:32 +0100

    tablica sekwencji

---
+++ +book/B_01_zaawansowany Python, wyd. 2/P_2-1.py
@@ -0,0 +1,8 @@
+symbols = '$¢£¥€¤'
+codes = []
+for symbol in symbols:
+    codes.append(ord(symbol))
+
+print(codes)
+
+# [36, 162, 163, 165, 8364, 164]
---
+++ +book/B_01_zaawansowany Python, wyd. 2/P_2-2.py
@@ -0,0 +1,7 @@
+symbols = '$¢£¥€¤'
+codes = [ord(symbol) for symbol in symbols]
+
+
+print(codes)
+
+# [36, 162, 163, 165, 8364, 164]
